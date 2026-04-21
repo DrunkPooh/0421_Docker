ef6a819aea132337f5aeeab7d23db16f823266a5
AH_03_신정호 <top03010@gmail.com>
2026-04-21 15:52:45 +0900

    0421_request.py

---
+++ user/request.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+# 회원가입 요청에 필요한 데이터 형식
+class SignUpRequest(BaseModel):
+    email: str
+    password: str
+
+
+# 로그인에 필요한 데이터 형식
+class LoginRequest(BaseModel):
+    email: str
+    password: str
